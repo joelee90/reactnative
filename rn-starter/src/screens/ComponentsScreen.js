393c8f9d2f9885c13b97b7bc5382cbd1fa8f7dc8
JongHyeokLee <jonghyeoklee90@gmail.com>
2020-07-03 12:36:50 +0900

    Add View with different styles

--- rn-starter/src/screens/ComponentsScreen.js
+++ rn-starter/src/screens/ComponentsScreen.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
 
 const ComponentsScreen = () => {
-  return <Text style={styles.textStyle}>This is Component Screen</Text>;
+  const name = 'Joe';
+  return (
+    <View>
+      <Text style={styles.textStyle}>Getting started with React Native!</Text>
+      <Text style={styles.textStyleMain}>My name is {name}</Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   textStyle: {
-    fontSize: 30,
+    fontSize: 45,
+  },
+  textStyleMain: {
+    fontSize: 20,
   },
 });
 
