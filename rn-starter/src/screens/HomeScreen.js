cb98c2c54c52453b6f4e93bdf01657c032e02daa
JongHyeokLee <jonghyeoklee90@gmail.com>
2020-07-07 23:54:17 +0900

    Section: 4 Button Types, Touchable Opacity, Navigation

--- rn-starter/src/screens/HomeScreen.js
+++ rn-starter/src/screens/HomeScreen.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text style={styles.text}>HomeScreen</Text>
+      <Button
+        title='Take Me To Components'
+        onPress={() => navigation.navigate('Components')}
+      />
+      <Button
+        title='Take Me To List'
+        onPress={() => navigation.navigate('List')}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
