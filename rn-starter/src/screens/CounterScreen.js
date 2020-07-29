d01e4f2b1532523b76a141589f1417d274fac14d
JongHyeokLee <jonghyeoklee90@gmail.com>
2020-07-29 14:26:05 +0900

    change CounterScreen.js from  useState to useReducer

--- rn-starter/src/screens/CounterScreen.js
+++ rn-starter/src/screens/CounterScreen.js
@@ -1,13 +1,36 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
+const COUNTER = 1;
+
+const reducer = (state, action) => {
+  // state === {counter : number}
+  // action === {type: 'increase' || 'decrease', payload: 1 || -1}
+  switch (action.type) {
+    case 'increase':
+      return { ...state, counter: state.counter + action.payload };
+    case 'decrease':
+      return { ...state, counter: state.counter + action.payload };
+    default:
+      return state;
+  }
+};
+
 const CounterScreen = () => {
-  const [counter, setCounter] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { counter: 0 });
+  const { counter } = state;
+
   return (
     <View>
       <Text style={styles.textStyle}>Current Count: {counter}</Text>
-      <Button title='+' onPress={() => setCounter(counter + 1)} />
-      <Button title='-' onPress={() => setCounter(counter - 1)} />
+      <Button
+        title='+'
+        onPress={() => dispatch({ type: 'increase', payload: COUNTER })}
+      />
+      <Button
+        title='-'
+        onPress={() => dispatch({ type: 'decrease', payload: -COUNTER })}
+      />
     </View>
   );
 };
